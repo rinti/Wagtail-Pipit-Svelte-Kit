bcb1938cf51b6e6d2318af89990ab82280fc86ac
Andreas Bernacca <hello@rinti.se>
2021-04-10 12:25:34 +0200

    Don't use api for now

--- src/routes/[...index].svelte
+++ src/routes/[...index].svelte
@@ -1,25 +1,27 @@
 <script context="module">
 	export async function load({ page, fetch, session, context }) {
-		const baseUrl = 'http://localhost:8081/wt/api/nextjs';
-		const url = `${baseUrl}/v1/page_by_path/?html_path=${page.path}`;
+		/* const baseUrl = 'http://localhost:8081/wt/api/nextjs'; */
+		/* const url = `${baseUrl}/v1/page_by_path/?html_path=${page.path}`; */
 
-		const headers = {
-			'Content-Type': 'application/json',
-			cookie: session.cookie
-		};
+		/* const headers = { */
+		/* 	'Content-Type': 'application/json', */
+		/* 	cookie: session.cookie */
+		/* }; */
 
-		const response = await fetch(url, { headers });
-		const payload = await response.json();
-		const containerName = payload.component_name;
+		/* const response = await fetch(url, { headers }); */
+		/* const payload = await response.json(); */
+		/* const containerName = payload.component_name; */
+        let containerName = 'ArticlePage'
 		let container;
 
-		if (containerName !== 'HomePage') {
+		if (containerName === 'HomePage') {
 			container = (await import('$lib/pages/HomePage.svelte')).default;
 		} else {
 			container = (await import('$lib/pages/ArticlePage.svelte')).default;
 		}
 
-		const props = payload.component_props;
+		/* const props = payload.component_props; */
+        const props = {}
 
 		return { props: { container, props } };
 	}
